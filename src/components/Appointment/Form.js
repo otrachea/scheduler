03349bfc2c7bc5897854cab017ae4fb2aa5bfcb2
otrachea <xtrachea@gmail.com>
2022-04-19 22:16:28 -0400

    State for student changes when user types in their name

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -13,14 +17,11 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-            your code goes here
-          */
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList
-        /* your code goes here */
+          
         />
       </section>
       <section className="appointment__card-right">
