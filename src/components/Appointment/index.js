bf098b9a7a5196ccfbf2c49a2f7e60619184da8a
otrachea <xtrachea@gmail.com>
2022-04-20 09:37:08 -0400

    Add Header, Show and Empty into Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,11 +2,17 @@ import React from "react";
 
 import "components/Appointment/style.scss";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      {!props.time && "No Appointments"}
-      {props.time && `Appointment at ${props.time}`}
+      <Header time={props.time} />
+      {props.interview ?
+        <Show></Show> :
+        <Empty></Empty>}
     </article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -167,4 +167,12 @@ storiesOf("Appointment", module)
     interviewers={interviewers}
     onSave={action("onSave")}
     onCancel={action("onCancel")}
-  />);
+  />)
+  .add("Appointment Empty", () => {
+    return (
+      <Fragment>
+        <Appointment id={1} time="4pm" />
+        <Appointment time="5pm" />
+      </Fragment>
+    );
+  });
