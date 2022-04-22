e3648b17556247ac642984b6efa58dcaa73c7d7a
otrachea <xtrachea@gmail.com>
2022-04-22 13:13:32 -0400

    Create custom hook, useVisualMode, that can so far set initial mode and transition to a new mode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+
+  const transition = (newMode) => {
+    setMode(newMode);
+  };
+
+  return { mode, transition };
+};
