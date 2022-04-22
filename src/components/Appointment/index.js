a92318bbdfb68d43740f1feca9f6f498c297b7cd
otrachea <xtrachea@gmail.com>
2022-04-22 13:57:02 -0400

    Use useVisualMode hook in Appointments component and create modes for SHOW and EMPTY

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,24 +2,33 @@ import React from "react";
 
 import "components/Appointment/style.scss";
 
+import useVisualMode from "hooks/useVisualMode";
+
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY &&
+        <Empty
+          onAdd={props.onAdd}
+        ></Empty>}
+      {mode === SHOW &&
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer.name}
           onEdit={props.onEdit}
           onDelete={props.onDelete}
-        ></Show> :
-        <Empty
-          onAdd={props.onAdd}
-        ></Empty>}
+        ></Show>}
     </article>
   );
 }
