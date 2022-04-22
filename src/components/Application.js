362f01aa00355559bdc33ebc6eebafd9e6bcc654
otrachea <xtrachea@gmail.com>
2022-04-22 10:41:24 -0400

    Create helper function getInterview

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Application";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
 
@@ -17,6 +17,17 @@ export default function Application(props) {
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   const setDay = day => setState(prev => ({ ...prev, day }));
 
@@ -25,8 +36,12 @@ export default function Application(props) {
     axios.get("/api/appointments"),
     axios.get("/api/interviewers")])
       .then(results => {
-        console.log(results[1].data);
-        setState(prev => ({ ...prev, days: results[0].data, appointments: results[1].data }));
+        setState(prev => ({
+          ...prev,
+          days: results[0].data,
+          appointments: results[1].data,
+          interviewers: results[2].data
+        }));
       });
   }, []);
 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,8 +1,20 @@
-export function getAppointmentsForDay(state, day) {
+export const getAppointmentsForDay = (state, day) => {
   const dayInState = state.days.filter(dayState => dayState.name === day)[0];
 
   if (!dayInState) return [];
 
   return dayInState.appointments.map(appointment => state.appointments[`${appointment}`]);
+};
 
-}
+export const getInterview = (state, interview) => {
+  return interview
+    ? {
+      student: interview.student,
+      interviewer: {
+        id: interview.interviewer,
+        name: state.interviewers[interview.interviewer].name,
+        avatar: state.interviewers[interview.interviewer].avatar
+      }
+    }
+    : null;
+};
